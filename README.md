d2588f9a56d0b2cc601f2f3069e62560f7cde235
Gabriel Carvalho <gabrielcarvalhos.snt@gmail.com>
2025-09-28 19:10:44 -0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# teste-codelab.dev
+ Teste para o processo seletivo da CodeLab.dev
---
+++ index.ts
@@ -0,0 +1,71 @@
+class VerifyString {
+    private _string: string;
+
+    constructor(s: string) {
+        this._string = s;
+    }
+
+    get string() {
+        return this._string;
+    }
+
+    set string(str: string) {
+        this._string = str;
+    }
+    
+    public validate() {
+        const stack: string[] = [];
+        const pairs: Map<string, string> = new Map([
+            [")", "("],
+            ["]", "["],
+            ["}", "{"]
+        ]);
+        
+        for (let i = 0; i < this._string.length; i++) {
+            if (this._string[i] == "(" || this._string[i] == "{" || this._string[i] == "[") {
+                stack.push(this._string[i]);
+            } else if (this._string[i] == ")" || this._string[i] == "}" || this._string[i] == "]") {
+                if (stack.length == 0) {
+                    return -1;
+                }
+                
+                const lastOpen = stack.pop();
+                const expected = pairs.get(this._string[i]);
+
+                if (lastOpen != expected) {
+                    return -1;
+                }
+            } else {
+                return -1;
+            }
+        }
+
+        return stack.length == 0 ? 0 : -1;
+    }
+}
+
+const input = require('prompt-sync')({ sigint: true });
+
+const str = input("Digite sua string: ");
+const validator = new VerifyString(str);
+
+console.log(validator.string, validator.validate());
+
+// TESTES
+validator.string = "()"
+console.log(validator.string, validator.validate());
+
+validator.string = "()[]{}"
+console.log(validator.string, validator.validate());
+
+validator.string = "(]"
+console.log(validator.string, validator.validate());
+
+validator.string = "([])"
+console.log(validator.string, validator.validate());
+
+validator.string = "([)]"
+console.log(validator.string, validator.validate());
+
+validator.string = "a("
+console.log(validator.string, validator.validate());
